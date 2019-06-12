18128cc1e5d33937b166ecaa95807c84b0bb7856
Daniel Schneider <daschne8@gmail.com>
2019-06-12 21:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function nameToAttributes(drivers){
   return drivers.map(function(driver){return {firstName: driver.split(' ')[0], lastName: driver.split(' ')[1]}})
 }
 function attributesToPhrase(drivers){
-  return drivers.map(function(drivers){ return `${driver.name} is from ${driver.hometown}` })
+  return drivers.map(function(driver){ return `${driver.name} is from ${driver.hometown}` })
 }
