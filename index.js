eb38e6f4801a7df5ae9d51785f1063649814bbc2
Daniel Schneider <daschne8@gmail.com>
2019-06-12 21:17:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function lowerCaseDrivers(drivers){
   return drivers.map(function(driver){return driver.toLowerCase()})
 }
 function nameToAttributes(drivers){
-  return drivers.map(function(driver){return {firstName: driver.split(' ')[0], lastName: driver.split(' ')[1]})
+  return drivers.map(function(driver){return {firstName: driver.split(' ')[0], lastName: driver.split(' ')[1]}})
 }
